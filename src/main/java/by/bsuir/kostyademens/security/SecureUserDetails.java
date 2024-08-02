8081773ac87b0758c6d6eabfc32fd16c4bae65fe
KostyaDemens <kostyademens@gmail.com>
2024-08-02 15:34:34 +0300

    feat: add SecureUserDetails class

---
+++ src/main/java/by/bsuir/kostyademens/security/SecureUserDetails.java
@@ -0,0 +1,52 @@
+package by.bsuir.kostyademens.security;
+
+import by.bsuir.kostyademens.model.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class SecureUserDetails implements UserDetails {
+
+    private final User user;
+
+    public SecureUserDetails(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return this.user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return this.user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+}
