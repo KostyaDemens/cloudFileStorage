33b2e2ef996f450177d354f4b2ad695394b652c2
KostyaDemens <kostyademens@gmail.com>
2024-08-06 20:37:26 +0300

    feat: add MinioConfig class which creates a bucket if it does not exist

--- src/main/java/by/bsuir/kostyademens/config/MinioConfig.java
+++ src/main/java/by/bsuir/kostyademens/config/MinioConfig.java
@@ -1,9 +1,16 @@
 package by.bsuir.kostyademens.config;
 
+import io.minio.BucketExistsArgs;
+import io.minio.MakeBucketArgs;
 import io.minio.MinioClient;
+import io.minio.errors.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 
 @Configuration
 public class MinioConfig {
@@ -17,11 +24,21 @@ public class MinioConfig {
     @Value("${minio.access.secret}")
     private String accessSecret;
 
+    @Value("${minio.bucket.name}")
+    private String bucketName;
+
     @Bean
-    public MinioClient minioClient() {
-        return MinioClient.builder()
+    public MinioClient minioClient() throws ServerException, InsufficientDataException, ErrorResponseException, IOException, NoSuchAlgorithmException, InvalidKeyException, InvalidResponseException, XmlParserException, InternalException {
+        MinioClient minioClient = MinioClient.builder()
                 .endpoint(url)
                 .credentials(accessKey, accessSecret)
                 .build();
+
+        boolean found = minioClient.bucketExists(BucketExistsArgs.builder().bucket(bucketName).build());
+        if (!found) {
+            minioClient.makeBucket(MakeBucketArgs.builder().bucket(bucketName).build());
+        }
+
+        return minioClient;
     }
 }
