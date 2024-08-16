49b2f337db9c880bbc4f68f1ad4f78e3ad93f11e
KostyaDemens <kostyademens@gmail.com>
2024-08-16 17:14:11 +0300

    feat: add ItemDownloadDto for downloading files

--- src/main/java/by/bsuir/kostyademens/controller/FileController.java
+++ src/main/java/by/bsuir/kostyademens/controller/FileController.java
@@ -1,28 +1,37 @@
 package by.bsuir.kostyademens.controller;
 
-import by.bsuir.kostyademens.service.ItemService;
+import by.bsuir.kostyademens.dto.ItemDownloadDto;
 import by.bsuir.kostyademens.service.SimpleStorageService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ContentDisposition;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.InputStream;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/file")
 public class FileController {
 
-
     private final SimpleStorageService storageService;
-    private final ItemService itemService;
-//    private final FolderService folderService;
 
-//    @SneakyThrows
-//    @GetMapping("download")
-//    public ResponseEntity<byte[]> download(@ModelAttribute ItemDto itemDto) {
-//        InputStream stream = storageService.getFile(itemDto.getFullPath());
-//        HttpHeaders httpHeaders = new HttpHeaders();
-//        httpHeaders.set(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_OCTET_STREAM_VALUE);
-//        httpHeaders.set(HttpHeaders.CONTENT_DISPOSITION, ContentDisposition.attachment().filename(itemDto.getName()).build().toString());
-//        return ResponseEntity.ok().headers(httpHeaders).body(stream.readAllBytes());
-//    }
+    @GetMapping("download")
+    public ResponseEntity<byte[]> download(@ModelAttribute ItemDownloadDto item) {
+        try {
+            InputStream stream = storageService.getFile(item.getPath());
+            HttpHeaders httpHeaders = new HttpHeaders();
+            httpHeaders.set(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_OCTET_STREAM_VALUE);
+            httpHeaders.set(HttpHeaders.CONTENT_DISPOSITION, ContentDisposition.attachment().filename(item.getName()).build().toString());
+            return ResponseEntity.ok().headers(httpHeaders).body(stream.readAllBytes());
+        } catch (Exception e) {
+            //TODO Обработать исключение
+            throw new RuntimeException();
+        }
+    }
 }
---
+++ src/main/java/by/bsuir/kostyademens/dto/ItemDownloadDto.java
@@ -0,0 +1,12 @@
+package by.bsuir.kostyademens.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ItemDownloadDto {
+
+    private String path;
+    private String name;
+}
